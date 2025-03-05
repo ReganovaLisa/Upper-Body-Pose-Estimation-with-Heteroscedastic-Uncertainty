a4fb68991491372a33640ef9261739f61035e17e
ReganovaLisa <reganova.ev@phystech.edu>
2025-03-05 15:37:35 +0100

    add calibration plot and output distribution functions

--- models/losses.py
+++ models/losses.py
@@ -180,4 +180,62 @@ class Mse_loss_beta_nll(tf.keras.losses.Loss):
         loss = 0.5 * beta_term * (mse +  reg)
 
         return loss
+    
+
+
+class Mse_loss_beta_nll_adj(tf.keras.losses.Loss):
+    def __init__(self, beta_coef=0.5, **kwargs):
+        """
+        Initialize the loss function with a beta coefficient.
+
+        Args:
+            beta_coef (float): Beta coefficient, default is 0.5.
+            **kwargs: Additional arguments for the base class.
+        """
+        super(Mse_loss_beta_nll_adj, self).__init__(**kwargs)
+        self.beta_coef = beta_coef
+
+    def call(self, y_true, y_pred):
+        """
+        Mean squared error loss with uncertainty for angles.
+
+        Args:
+            y_true (tf.Tensor): Ground truth values. Shape: (batch_size, 2)
+            y_pred (tf.Tensor): Predicted values. Shape: (batch_size, 2)
+
+        Returns:
+            tf.Tensor: Computed loss.
+        """
+
+        # Ensure y_pred has the correct shape
+        tf.debugging.assert_equal(tf.shape(y_pred)[-1], 2, "y_pred must have 2 elements per sample: [angle, uncertainty]")
+
+        # Extract predicted angle and uncertainty
+        uncertainty = y_pred[:, 1]
+        y_pred_angle = y_pred[:, 0]
+
+        # Extract ground truth angle
+        cont_true = y_true[:, 0]
+
+        # Compute squared error
+        squared_error = tf.math.square(cont_true - y_pred_angle)
+
+        # Compute inverse standard deviation with clipping for stability
+        inv_std = tf.math.exp(-uncertainty)
+
+        # Compute beta term where beta is const between 0 and 1
+        beta_term = tf.math.exp(self.beta_coef * uncertainty)
+
+        # Compute MSE weighted by uncertainty
+        mse = tf.reduce_mean(inv_std * squared_error)
+
+        # Regularization term
+        reg = tf.reduce_mean(uncertainty)
+
+        # Debugging (optional)
+        #tf.print("MSE:", mse, "Regularization:", reg)
+
+        loss = 0.5 * beta_term * (mse +  reg)
+
+        return loss
 
